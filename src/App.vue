477442e8876e7cc0e7e939d6031cf639aaae68a2
Micaela-ux <ruiz.mica@hotmail.com>
2023-07-05 03:17:47 -0300

    add: vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,72 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="ligth"
+      white
+    >
+      <div class="d-flex align-center">
+        <span>MS</span>
+      </div>
+
+      <v-spacer>
+    
+      </v-spacer>
+
+      <v-btn
+        text
+      >
+        <span class="mr-2">Iniciar sesión</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main class="text-center">
+        
+      
+          <v-list-item-content>
+              <v-list-item-title>
+                <router-link to="/">
+                Home
+                </router-link>
+              </v-list-item-title>
+            </v-list-item-content>
+
+            <v-list-item-content>
+              <v-list-item-title>
+                <router-link to="/ingresar">
+                 Ingresar
+                </router-link>
+              </v-list-item-title>
+            </v-list-item-content>
+          
+
+         <!-- <v-list-item link to="/ingresar">
+            <v-list-item-title>Ingresar</v-list-item-title>
+          </v-list-item>
+-->
+
+       
+    </v-main>
+
+   
+
+    <v-container>
+       <router-view/>
+    </v-container>
+
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+export default {
+  name: 'App',
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+     //
+  }),
+};
+</script>
+
