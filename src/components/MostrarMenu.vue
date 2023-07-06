ca2c8293d2fe574c775285a0940285ac2257762f
Micaela-ux <ruiz.mica@hotmail.com>
2023-07-06 03:20:19 -0300

    shore: components MostrarMenu

--- src/components/MostrarMenu.vue
+++ src/components/MostrarMenu.vue
@@ -1,49 +1,34 @@
 <template>
-<v-app>
-  <v-table>
-    <thead>
-      <tr>
-        <th class="text-center">
-          Día
-        </th>
-        <th class="text-center">
-          Horario
-        </th>
-        <th class="text-center">
-          Menú
-        </th>
-        <th class="text-center">
-          Postre
-        </th>
-        <th class="text-center">
-          Acciones
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="item in desserts"
-        :key="item.name"
-      >
-        <td>{{ item.name }}</td>
-        <td>{{ item.calories }}</td>
-        <td>{{ item.name }}</td>
-        <td>{{ item.name }}</td>
-        <td>
-          <v-btn
-            color="error"
-            class="mt-2"
-            block
-            @click="reset"
-          >
-            Eliminar
-          </v-btn>
-        </td>
-      </tr>
-    </tbody>
-  </v-table>
- 
- </v-app>
+ <div>
+
+  <v-card
+      v-for="item in local"
+      :key="item.id"
+  >
+    <v-card-text>
+      <div>Horario: {{item.horario}} hs.</div>
+      <p class="text-h4 text--primary">
+        {{item.dia}}
+      </p>
+      <div class="text--primary">
+       Menú: {{item.menu}}
+      </div>
+      <div class="text--primary">
+       Postre: {{item.postre}}
+      </div>
+    </v-card-text>
+    <v-card-actions class="btn-delete">
+        <v-btn
+          color="error"
+          class="mt-2"
+          @click="eliminar(item)"
+        >
+          Eliminar
+        </v-btn>
+      </v-card-actions>
+  </v-card>
+
+ </div>
 </template>
 
 
@@ -51,65 +36,48 @@
 
   export default {
 
-    data () {
-      return {
+    data: () => ({
+      
+      name: 'MostrarMenu',
 
-      titulo: "Mi menú semanal",
+      local:[],
 
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-          },
-        ],
-      }
+    }),
+
+    mounted:function(){
+      console.log("se monto");
+      this.ver_local();
     },
+
+    methods:{
+      ver_local:function(){
+        
+        if(localStorage.dato){
+          this.local=JSON.parse(localStorage.getItem("dato"));     
+        }
+      },
+
+      eliminar(x) {
+        this.local.splice(x, 1);
+        this.saveDato();
+      },
+
+      saveDato() {
+      const parsed = JSON.stringify(this.local);
+      localStorage.setItem('dato', parsed);
+      }
+
+    }
+
   }
 </script>
 
 <style>
-  .header{
-
-    padding: 5%;
-    background-color: #8c0202;
-    text-align: center;
-    color: white;
-
+  .btn-delete{
+    display: flex;
+    padding: 8px;
+    justify-content: flex-end;
+    align-items: center;
   }
+  
 </style>
