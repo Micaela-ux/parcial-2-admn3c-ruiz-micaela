4bef6094113c1e17ff7174731e90120b13c00d6c
Micaela-ux <ruiz.mica@hotmail.com>
2023-07-06 03:21:02 -0300

    add: form and methods

--- src/components/IngresarMenu.vue
+++ src/components/IngresarMenu.vue
@@ -1,78 +1,79 @@
 <template>
-  <v-app>
-    <v-form @submit.prevent="submit">
-      <v-container>
-        <v-row>
-          <v-col
-            cols="12"
-            md="6"
-          >
-            <v-select
-              v-model="select"
-              :items="items"
-              :rules="[v => !!v || 'Seleccione un día.']"
-              label="Día: "
-              required
-            ></v-select>
-          </v-col>
-
-          <v-col
-            cols="12"
-            md="6"
-          >
-            <v-text-field
-              v-model="horario"
-              :rules="obligatorio"
-              label="Horario: "
-              required
-            ></v-text-field>
-          </v-col>
-        </v-row>
-
-        <v-row>
-          <v-col
-            cols="12"
-            md="6"
-          >
-            <v-text-field
-              v-model="menu"
-              :rules="obligatorio"
-              label="Menú: "
-              required
-            ></v-text-field>
-          </v-col>
-
-          <v-col
-            cols="12"
-            md="6"
-          >
-            <v-text-field
-              v-model="postre"
-              :rules="obligatorio"
-              label="Postre: "
-              required
-            ></v-text-field>
-          </v-col>
-        </v-row>
-
-        <v-row>
-          <v-col
+  <v-form @submit.prevent>
+    <v-container>
+      <v-row>
+        <v-col
           cols="12"
-          >
-            <v-btn
-                @click="agregar(form_data)"
-              >
-                Agregar
-              </v-btn>
-          </v-col>
-        </v-row>
+          md="6"
+        >
+          <v-select
+            v-model="form_data.dia"
+            :items="items"
+            :rules="[v => !!v || 'Seleccione un día.']"
+            label="Día: "
+            required
+          ></v-select>
+        </v-col>
 
-      </v-container>
-    </v-form>
-  </v-app>
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <v-text-field
+            v-model="form_data.horario"
+            :rules="obligatorio"
+            label="Horario: "
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <v-text-field
+            v-model="form_data.menu"
+            :rules="obligatorio"
+            label="Menú: "
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <v-text-field
+            v-model="form_data.postre"
+            :rules="obligatorio"
+            label="Postre: "
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col
+        cols="12"
+        >
+          <v-btn
+              @click="agregar(form_data)"
+            >
+              Agregar
+            </v-btn>
+        </v-col>
+      </v-row>
+
+    </v-container>
+  </v-form>
 </template>
 
 <script>
+  
+  var menus = [];
+
   export default {
     data: () => ({
       name: 'IngresarMenu',
@@ -97,8 +98,31 @@
           return 'Completar, campo obligatorio.'
         },
       ],
-    
+
+      form_data:{
+        dia: "",
+        horario: "",
+        menu: "",
+        postre: "",
+      },
+
     }),
 
-  } 
+    methods:{
+      agregar:function(form_data){
+        if(!localStorage.dato){
+          menus=[];
+        }else{
+          menus=JSON.parse(localStorage.getItem("dato"));
+        }
+
+        menus.push(form_data);
+        localStorage.setItem("dato",JSON.stringify(menus));
+      
+        this.$router.push('/');
+
+      }
+
+    },
+	}
 </script>
